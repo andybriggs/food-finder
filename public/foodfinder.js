38cd9bdd5f5579c74addafef8fe49a297abaab5c
Andy <andybriggs84@gmail.com>
2016-12-07 20:03:42 +0000

    built out form dynamically

--- public/foodfinder.js
+++ public/foodfinder.js
@@ -4,23 +4,42 @@ var FoodFinder = (function () {
 
   var module = {};
 
-  var buildVenuListItem = function(venue) {
+  var buildVenueListItem = function(venue) {
     var listItem = document.createElement('li');
-    listItem.appendChild(document.createTextNode(venue));
+    listItem.appendChild(document.createTextNode(venue.name));
     return listItem;
   };
 
-  var buildVenueList = function(venues) {
+  var buildUserListItem = function(user, id) {
+    console.log(user);
+    var listItem = document.createElement('li'),
+    checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.value = user.name;
+    checkbox.id = 'chk' + id;
+
+    var label = document.createElement('label')
+    label.htmlFor = 'chk' + id;
+    label.appendChild(document.createTextNode(user.name));
+
+    listItem.appendChild(checkbox);
+    listItem.appendChild(label);
+
+    return listItem;
+  };
+
+  var buildList = function(listItems, listItemBuilder) {
     var newList = document.createElement('ul');
-    for (var i = 0; i < venues.length; i++) {
-      var row = buildVenuListItem(venues[i].name);
+    for (var i = 0; i < listItems.length; i++) {
+      var row = listItemBuilder(listItems[i], i);
       newList.appendChild(row);
     };
     document.getElementById('app').appendChild(newList);
   };
 
   var buildForm = function(data) {
-    buildVenueList(data.venues);
+    buildList(data.venues, buildVenueListItem);
+    buildList(data.users, buildUserListItem);
   }
 
   module.run = function() {
