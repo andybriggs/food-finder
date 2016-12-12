8a4118896bc6b62b5966d742fc78727efa623561
Andy Briggs <andy.briggs@dctfh.com>
2016-12-12 15:38:20 +0000

    changed app module config to be private

--- public/app.js
+++ public/app.js
@@ -2,18 +2,15 @@
 
 var App = (function () {
 
-  var module = {
-    receivedAppData: false,
-    appData: {
-      attendees: [],
-      venues: [],
-      foodPrefs: []
-    }
-  };
+  var module = {},
+  users,
+  venues,
+  foodPrefs = [],
+  attendees = [],
+  receivedAppData = false;
 
   var getUserData = function(userName) {
-    var users = module.appData.users,
-    user;
+    var user;
     for(var i = 0; i < users.length; i++) {
       if(users[i].name === userName) {
         user = users[i];
@@ -23,21 +20,14 @@ var App = (function () {
   };
 
   var checkVenues = function() {
-    var venues = module.appData.venues,
-    foodPrefs = module.appData.foodPrefs;
     for(var i = 0; i < venues.length; i++) {
       var conflicts = 0;
-      console.log('==============================');
-      console.log('Venue: ' + venues[i].name);
       for(var x = 0; x < venues[i].food.length; x++) {
-        console.log('Compare : ' + venues[i].food[x]);
         for(var y = 0; y < foodPrefs.length; y++) {
-          console.log('with: ' + foodPrefs[y]);
           if(venues[i].food[x] === foodPrefs[y]) {
             conflicts ++;
-            console.log(conflicts + ' conflict(s): ' + foodPrefs[y]);
             if(conflicts === venues[i].food.length) {
-              console.log('=================>' + venues[i].name);
+              console.log('=================> ' + venues[i].name);
             }
             break;
           }
@@ -47,19 +37,14 @@ var App = (function () {
   };
 
   var updatePrefs = function() {
-    var venues = module.appData.venues,
-    attendees = module.appData.attendees,
-    prefs = module.appData.foodPrefs;
-
     for(var i = 0; i < attendees.length; i++) {
       for(var x = 0; x < attendees[i].wont_eat.length; x++) {
-        prefs.push(attendees[i].wont_eat[x]);
+        foodPrefs.push(attendees[i].wont_eat[x]);
       }
     }
   };
 
-  var updateAtendees = function(user, checked) {
-    var attendees = module.appData.attendees;
+  var updateAttendees = function(user, checked) {
     if(checked) {
       attendees.push(user);
     } else {
@@ -72,16 +57,15 @@ var App = (function () {
   };
 
   var updateApp = function(el) {
-    if(!module.receivedAppData) {
-      module.appData.users = FormBuilder.formData.users;
-      module.appData.venues = FormBuilder.formData.venues;
+    if(!receivedAppData) {
+      users = FormBuilder.formData.users;
+      venues = FormBuilder.formData.venues;
     }
     var user = getUserData(el.value);
-    module.receivedAppData = true;
-    updateAtendees(user, el.checked);
+    receivedAppData = true;
+    updateAttendees(user, el.checked);
     updatePrefs();
     checkVenues();
-    console.log(module.appData);
   };
 
   module.run = function() {
